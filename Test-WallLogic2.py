75d03aaffd2982b0cc995a9544fb33ae9ca7ead8
JuanLeycal <leyva.calle1403@gmail.com>
2020-11-27 22:48:18 -0500

    2nd greedy algorithm
    i do not know how to say algoritmo in english

---
+++ Test-WallLogic2.py
@@ -0,0 +1,14 @@
+#Lo que se busca es, según la lista de movimientos a hacer del peón, poner paredes usando la lista de movimientos de los peones
+
+posPeon1Y = 0
+posPeon1X = 4
+
+posPeon2Y = 8
+posPeon2X = 4
+
+posPeon1Mov = []
+posPeon2Mov = []
+
+def ruta(name1, name2, meta1, meta2):
+    if(name1):
+        
